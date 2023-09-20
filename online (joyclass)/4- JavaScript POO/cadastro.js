c70e2be6b9724912b4593bae5ef8bf7496f30998
adrielcorreia <adrielcostacorreia54@gmail.com>
2023-09-20 14:59:30 -0300

    cadastro

---
+++ online (joyclass)/4- JavaScript POO/cadastro.js
@@ -0,0 +1,6 @@
+const userName = "Adriel Correia da Costa";
+let salary = 4239.00.toFixed(2);
+let age = 20;
+let diploma = true;
+
+console.log(`Nome: ${userName}; \nSalário: R$${salary}; \nIdade: ${age} anos; \nDiplomado: ${diploma}.`);
