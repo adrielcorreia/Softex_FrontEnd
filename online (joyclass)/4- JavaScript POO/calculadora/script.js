60bcdce358e4ac2acd00ddde3eb74b04595e95b7
adrielcorreia <adrielcostacorreia54@gmail.com>
2023-09-25 16:50:39 -0300

    calculadora

--- online (joyclass)/4- JavaScript POO/calculadora/script.js
+++ online (joyclass)/4- JavaScript POO/calculadora/script.js
@@ -1,1 +1,60 @@
-n1 = 2;
+const display = document.querySelector(".display");
+let opr = '';
+let arr = [];
+let result = 0;
+
+function guardar(e) {
+    display.innerHTML += `${e}`;
+}
+
+function soma() {
+    opr = 'soma';
+    arr.push(display.innerHTML);
+    display.innerHTML = '';
+    console.log(arr);
+}
+
+function subtrair() {
+    opr = 'subtrair';
+    arr.push(display.innerHTML);
+    display.innerHTML = '';
+    console.log(arr);
+}
+
+function multiplicar() {
+    opr = 'multiplicar';
+    arr.push(display.innerHTML);
+    display.innerHTML = '';
+    console.log(arr);
+}
+
+function dividir() {
+    opr = 'dividir';
+    arr.push(display.innerHTML);
+    display.innerHTML = '';
+    console.log(arr);
+}
+
+
+function resultado() {
+    arr = arr.map((x) => parseInt(x))
+
+    if (opr == 'soma') {
+        result = arr.reduce((a, v) => a+v);
+    }
+
+    if (opr == 'subtrair') {
+        result = arr.reduce((a, v) => a-v);
+    }
+
+    if (opr == 'multiplicar') {
+        result = arr.reduce((a, v) => a*v); 
+    }
+
+    if (opr == 'dividir') {
+        result = arr.reduce((a, v) => a/v); 
+    }
+
+    console.log(result)
+    display.innerHTML = result;
+}
