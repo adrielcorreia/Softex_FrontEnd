f118fc091555447f3fc677e6c01b82cacf1718a8
adrielcorreia <adrielcostacorreia54@gmail.com>
2023-09-27 12:27:35 -0300

    functions-examples

---
+++ online (joyclass)/4- JavaScript POO/funções/main.js
@@ -0,0 +1,18 @@
+let num1 = 5;
+let num2 = 2;
+
+function print() {
+    return `os números são: ${num1} e ${num2}`
+}
+
+function somar(n1, n2) {
+    return `${n1} + ${n2} = ${n1+n2}`
+}
+
+expoente = (n1,n2) => {
+    return `${n1}² = ${n1**n2}`
+}
+
+console.log(print())
+console.log(somar(num1, num2))
+console.log(expoente(num1, num2))
