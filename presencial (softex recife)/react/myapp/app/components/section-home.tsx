4d0f3005e533ff4cb725dd64ae8b0b6925dd9c75
adrielcorreia <adrielcostacorreia54@gmail.com>
2023-10-28 00:08:38 -0300

    static-done

--- presencial (softex recife)/react/myapp/app/components/section-home.tsx
+++ presencial (softex recife)/react/myapp/app/components/section-home.tsx
@@ -1,11 +1,24 @@
+"use client"
+
+import { useState } from 'react';
 import '../styles/buttons.css';
 
 export function SquareAndButton() {
+    const [count, setCount] = useState(1)
+
+    const increment = (type: React.MouseEvent<HTMLElement>) => {
+        setCount((prevState) => prevState + 1)
+    }
+
     return (
         <section className="section homeSquare">
             <div className='div-section'>
-                <div className="square"></div>
-                <button className="btn">Clique</button>
+                <div className="square">
+                    <div id='number-count'>
+                        <p>{count}</p>
+                    </div>               
+                </div>
+                <button onClick={increment} className="btn" >Clique</button>
             </div>
         </section>
     )
